5901c3bb276baf547633dbb9bdf34d5ecb09736e
chanik <whcksdlr5909@gmail.com>
2023-05-18 14:06:17 +0900

    "fetch-jo 브랜치 생성!"

---
+++ final-pjt-front/src/views/MovieView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="justify-content-center">
+    <MovieCard />
+  </div>
+</template>
+
+<script>
+import MovieCard from '@/components/MovieCard.vue'
+
+export default {
+  name: 'movieView',
+  methods: {
+    getMovies() {
+      this.$store.dispatch('getMovies')
+    }
+  },
+  components: {
+    MovieCard
+  },
+  created() {
+    this.getMovies();
+  },
+  computed: {
+    movieList() {
+      return this.$store.state.movieList
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
